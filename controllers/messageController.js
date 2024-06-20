31e4114220c1a4aba0e8b8c14c9d07776f7f72e0
Vishnu Gajulapalli <110832093+vishnu-blkbox@users.noreply.github.com>
2024-06-20 16:20:29 +0530

    add message routes, schemas, controllers

---
+++ controllers/messageController.js
@@ -0,0 +1,88 @@
+import Conversation from "../models/conversationModel.js";
+import Message from "../models/messageModel.js";
+
+export async function sendMessage(req, res) {
+    try {
+        const { recipientId, message } = req.body;
+        const senderId = req.user._id;
+
+        let conversation = await Conversation.findOne({
+            participants: { $all: [senderId, recipientId] }
+        });
+
+        if (!conversation) {
+            conversation = new Conversation({
+                participants: [senderId, recipientId],
+                lastMessage: {
+                    senderId: senderId,
+                    text: message,
+                }
+            });
+            await conversation.save();
+        }
+
+        const newMessage = await Message({
+            conversationId: conversation._id,
+            senderId: senderId,
+            text: message,
+        });
+
+        await Promise.all([
+            newMessage.save(),
+            conversation.updateOne({
+                lastMessage: {
+                    senderId: senderId,
+                    text: message,
+                }
+            })
+        ]);
+
+        res.status(201).json(newMessage);
+
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ error: error });
+    }
+}
+
+export async function getMessages(req, res) {
+    const { otherUserId } = req.params;
+    const userId = req.user._id;
+
+    try {
+        const conversation = await Conversation.findOne({
+            participants: { $all: [userId, otherUserId] }
+        });
+
+        if (!conversation) {
+            return res.status(404).json({ error: 'Conversation not found' });
+        }
+
+        const messages = await Message.find({
+            conversationId: conversation._id
+        }).sort({ createdAt: 1 });
+
+        return res.status(200).json(messages);
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ error: error });
+    }
+}
+
+export async function getConversations(req, res) {
+    const userId = req.user._id;
+
+    try {
+        const conversations = await Conversation.find({
+            participants: userId
+        }).populate({
+            path: 'participants',
+            select: 'username profilePic'
+        }).sort({ updatedAt: -1 });
+
+        return res.status(200).json(conversations);
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ error: error });
+    }
+}
